0a9a3d8a43354bb30aac3bf6424d0b73b850fdf1
Lucas Constantino <lucasconstantino0102@gmail.com>
2026-02-27 11:37:44 -0300

    build

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ echo "🚀 Iniciando o setup do Flutter na Vercel..."
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 2. Habilita o web e baixa as dependências
+# 2. Habilita o web, remove o lock corrompido e baixa dependências
 flutter config --enable-web
+rm pubspec.lock # <-- Adicionamos isso para forçar uma instalação limpa!
 flutter pub get
 
 echo "🔐 Gerando arquivo .env..."
 
-# 3. Cria o arquivo .env usando as variáveis injetadas pela Vercel
-# Substitua "BASE_URL" e "API_KEY" pelos nomes exatos que você usa no seu .env
-echo "BASE_URL=$URL_HOMOL" > .env
-# Use '>' para a primeira linha e '>>' para as próximas linhas!
+# 3. Cria o arquivo .env
+echo "BASE_URL=$BASE_URL" > .env
+# Se tiver mais variáveis, adicione com >> aqui
 
 echo "🏗️ Fazendo o build do projeto..."
 
